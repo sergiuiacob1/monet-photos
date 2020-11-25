4aed47c22b79b1f4504e2fc0fde98c3bf6976567
Adrian Gotca <adriangotca98@gmail.com>
2020-11-25 08:43:08 +0200

    Added gcloud app and implemented the split for an image

--- python/backend/image_transformation.py
+++ python/backend/image_transformation.py
@@ -1,5 +1,6 @@
 import cv2
-
+import numpy as np
+from exceptions import InvalidRequest
 
 def resize_image(img):
     dim = (256, 256)
@@ -7,8 +8,31 @@ def resize_image(img):
 
 
 def segment_image(image, size):
-    return 1, []
+    img_height = image.shape[0]
+    img_width = image.shape[1]
+
+    if img_height%size!=0 or img_width%size!=0:
+        raise InvalidRequest('Image dimensions don\'t divide by block size')
+    
+    heigth_blocks = img_height//size
+    width_blocks = img_width//size
+
+    response = []
+    for i in range(0,heigth_blocks):
+        response.append([])
+    
+    for i in range(0,heigth_blocks):
+        for j in range(0, width_blocks):
+            x = i*size
+            y = j*size
+            x1 = x+size
+            y1 = y+size
+            rectangle = image[x:x1,y:y1]
+
+            response[i].append(rectangle)
+
+    return (heigth_blocks, width_blocks), response
 
 
 def reconstruct_image(image_segments, shape, size):
-    return []
+    pass
