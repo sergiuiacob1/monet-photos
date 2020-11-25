82fd5292c2c4d462010dd4b1582e0872b2ff304a
Sergiu Iacob <sergiu.iacob1@gmail.com>
2020-11-25 08:18:12 +0200

    add bloc MOP

---
+++ mobile/lib/bloc/simple_bloc_observer.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    print("Got event: $event");
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print("Transition between states: $transition");
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(dynamic bloc, Object error, StackTrace stackTrace) {
+    print("Bloc error: $error");
+    super.onError(bloc, error, stackTrace);
+  }
+}
