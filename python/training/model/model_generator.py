de5d7b584f89af42843176a6061ad9beb6bd5d8d
Lucian Bosinceanu <lucian@shoreline.io>
2021-01-04 21:38:54 +0200

    Replaced model batch normalization with instance normalization

--- python/training/model/model_generator.py
+++ python/training/model/model_generator.py
@@ -5,7 +5,7 @@ from keras.layers import Conv2DTranspose
 from keras.layers import Activation
 from keras.initializers import RandomNormal
 from keras.layers import Concatenate
-from keras.layers import BatchNormalization
+from tensorflow_addons.layers import InstanceNormalization
 
 
 def resnet_block(n_filters, input_layer):
@@ -13,11 +13,11 @@ def resnet_block(n_filters, input_layer):
     init = RandomNormal(stddev=0.02)
     # first layer convolutional layer
     g = Conv2D(n_filters, (3, 3), padding='same', kernel_initializer=init)(input_layer)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # second convolutional layer
     g = Conv2D(n_filters, (3, 3), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     # concatenate merge channel-wise with input layer
     g = Concatenate()([g, input_layer])
     return g
@@ -30,30 +30,30 @@ def define_generator(image_shape=(256, 256, 3), n_resnet=9):
     in_image = Input(shape=image_shape)
     # c7s1-64
     g = Conv2D(64, (7, 7), padding='same', kernel_initializer=init)(in_image)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # d128
     g = Conv2D(128, (3, 3), strides=(2, 2), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # d256
     g = Conv2D(256, (3, 3), strides=(2, 2), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # R256
     for _ in range(n_resnet):
         g = resnet_block(256, g)
     # u128
     g = Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # u64
     g = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     g = Activation('relu')(g)
     # c7s1-3
     g = Conv2D(3, (7, 7), padding='same', kernel_initializer=init)(g)
-    g = BatchNormalization(axis=-1)(g)
+    g = InstanceNormalization(axis=-1)(g)
     out_image = Activation('tanh')(g)
     # define model
     model = Model(in_image, out_image)
